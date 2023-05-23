1332deff7f60f966c6a382f54dadec254b3ffe33
Daniel Costa <daniel.costa@knowledgebiz.pt>
2023-05-23 13:33:43 +0100

    Update

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI(
     ),
     version="1.0.0",
     title="API SINGULAR ROUTE",
-    description="This API integrates with SMARTCARING system"
+    description="This API integrates with SMART-CARING system"
 )
 
 
