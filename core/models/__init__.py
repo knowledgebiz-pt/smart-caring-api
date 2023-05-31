1f12b56bbe8045375b3bb7e88d8e4ea5a9abffd4
ruben-lebre <raplebre8@gmail.com>
2023-05-31 16:10:36 +0100

    Fixed merge issues

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,1 +1,3 @@
-from core.models import user_model, news_model, likes_model
+from core.models import user_model
+from core.models import news_model
+from core.models import likes_model
--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,3 @@
-from database import user_database, news_database, likes_database
+from database import user_database 
+from database import news_database
+from database import likes_database
