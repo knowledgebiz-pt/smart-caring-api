d95a2fef0a6c956a99522aafe118468c825fd8e3
Daniel Costa <daniel.costa@knowledgebiz.pt>
2023-06-28 10:39:13 +0100

    Toolbox Router updated

--- core/schemes/toolbox_schemes.py
+++ core/schemes/toolbox_schemes.py
@@ -5,40 +5,9 @@ from pyasn1.compat.octets import null
 from fastapi import Depends
 
 
-class ToolBoxPost(BaseModel):
-    """
-    ToolBox Post
-    """
-    _id: str
-    tool_name: str = Field(
-        ..., title="Name of the tool", min_length = 3, max_length=32
-    )
-    tool_description: str = Field(
-        ..., title="Description of the tool", min_length = 8, max_length=128
-    )
-    tool_image: Optional[str] = Field(default=null, alias="tool image")
-    tool_languages: Optional[str] = Field(default=null, alias="tool languages")
-
-
-class ToolBoxCreateResponse(BaseModel):
-    """
-    Toolbox create response
-    """
-    msg: str
-    data: object = {}
-
-
 class ToolBoxGetResponse(BaseModel):
     """
     Toolbox get response scheme
-    """
-    msg: str
-    data: object = {}
-
-
-class ToolBoxDeleteResponse(BaseModel):
-    """
-    Toolbox delete response scheme
     """
     msg: str
     data: object = {}
--- routers/toolbox_router.py
+++ routers/toolbox_router.py
@@ -4,19 +4,6 @@ import database.toolbox_database
 
 router = APIRouter()
 
-
-@router.post("/",
-             summary="Creates a new Tool Box",
-             description="Creates a new Tool Box",
-             response_description="Creates a new Tool Box",
-             response_model=core.schemes.toolbox_schemes.ToolBoxGetResponse,
-             operation_id="CreateToolBox"
-             )
-async def create_toolbox(tool: core.schemes.toolbox_schemes.ToolBoxPost):
-    response = database.toolbox_database.add_toolbox(tool)
-    return {"msg": "success",
-            "data": {response}}
-
 @router.get("/by-id/{id_toolbox}",
              summary="Return Tool Box by id_toolbox",
              description="Return Tool Box by id_toolbox",
@@ -34,14 +21,3 @@ async def get_by_id(response: Response, id_toolbox: str):
             "msg": "success",
             "data": response_database
             }
-
-@router.delete("/by-id/{id_toolbox}",
-               summary="Delete Tool Box by id_toolbox",
-               description="Delete Tool Box by id_toolbox",
-               response_description="Delete Tool Box by its ID",
-               response_model=core.schemes.toolbox_schemes.ToolBoxDeleteResponse,
-               operation_id="DeleteToolboxByIdToolBox"
-               )
-async def delete_by_id(response: Response, id_toolbox: str):
-    response_database = database.toolbox_database.delete_toolbox_by_id(id_toolbox)
-    return {"msg": "success", "data": []}
