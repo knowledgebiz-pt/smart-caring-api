33f5ca754b2357ea46c734a78e08c0214e2ed03c
oscar-silva <oscar.silva@knowledgebiz.pt>
2023-07-11 12:10:27 +0100

    Added clients test

--- client_websocket.py
+++ client_websocket.py
@@ -1,11 +1,12 @@
-from websocket import create_connection
+from websocket import create_connection, WebSocketApp
+import json
 
-ws = create_connection("ws://127.0.0.1:8000/")
-print(ws.recv())
+ws = create_connection("ws://192.168.1.170:8000/private-chat", header=["id-user: 12345"])
 print("Sending 'Hello, World'...")
-ws.send("Hello, World")
+ws.send(json.dumps({"id_user": "12345", "id_user_receiver": "1234", "message": "Olá Oscar"}))
 print("Sent")
 print("Receiving...")
 result = ws.recv()
+ws.send(json.dumps({"id_user": "12345", "id_user_receiver": "1234", "message": "Olá Oscar"}))
 print("Received '%s'" % result)
 ws.close()
