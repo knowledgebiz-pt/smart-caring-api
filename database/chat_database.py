7dbfebbec510764c208f693fabfc4a40a39c4102
Daniel Costa <daniel.costa@knowledgebiz.pt>
2023-07-12 15:00:36 +0100

    Updated chat model to save message to database

--- database/chat_database.py
+++ database/chat_database.py
@@ -33,18 +33,24 @@ def add_chat(value):
         message={
             "type": "msg",
             "content": value["message"],
+            "date": value["date"],
+            "id_user_sender": value["user_id_sender"],
+            "id_user_receiver": value["user_id_receiver"],
+            "deleted": value["deleted"],
+            "viewed": value["viewed"],
+            "sent": value["sent"]
         }
     ).save()
     return str(response.auto_id_0)
 
 
 def return_chat_by_id(id_chat):
-    response = model.chat_model.objects(_id=id_chat).first()
+    response = model.chat_model.Chat.objects(_id=id_chat).first()
     response = json.loads(response.to_json()) if response is not None else None
     return response
 
 
 def return_chat_by_user_id(id_user):
-    response = model.chat_model.objects(user_id=id_user)
+    response = model.chat_model.Chat.objects(user_id=id_user)
     response = json.loads(response.to_json()) if response is not None else None
     return response
--- web-socket-client.py
+++ web-socket-client.py
@@ -3,6 +3,6 @@ import json
 
  
 
-ws = create_connection("ws://127.0.0.1:8000/private-chat", header=["id-user: 12345"])
-ws.send(json.dumps({"user_id_sender": "12345", "user_id_receiver": "12345", "message": "Olá Oscar"}))
+ws = create_connection("ws://127.0.0.170:8000/private-chat", header=["id-user: 12345"])
+ws.send(json.dumps({"user_id_sender": "12345", "user_id_receiver": "12345", "message": "Olá Oscar", "date": "04-03-2023", "deleted": False, "viewed": True, "sent": True, "id_user_sender": "12345", "id_user_receiver": "12345"}))
 ws.close()
